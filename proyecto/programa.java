217b0e07ff940ae63648d4515bb5ef9093b0ef79
Arnau <arnaulopez26@gmail.com>
2019-10-08 13:17:13 +0000

    primera modificacion

--- proyecto/programa.java
+++ proyecto/programa.java
@@ -1,3 +1,4 @@
 a ver si acabo ya de una vez la practica esta joder que es una mierd ay cuesta que todo
 funcione unabarbaridad porfavor que haya algo mejor ya XDDD
 12345789012345780'1235690'
+asdghjklkhfdsdfhjkhfdsdghobvwpeu
